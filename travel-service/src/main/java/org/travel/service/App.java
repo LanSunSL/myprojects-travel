4bc6b19bf5672bfcc0dc270956c35bc3a5663325
'lans' <'nalnus@qq.com'>
2017-06-20 08:49:55 +0800

    New project: travel

---
+++ travel-service/src/main/java/org/travel/service/App.java
@@ -0,0 +1,13 @@
+package org.travel.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
